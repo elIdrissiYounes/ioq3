e0b13672582e08f3816d27c89db6463d17d5e819
Andrei Homescu <ah@immunant.com>
2019-12-12 15:10:42 -0800

    Enable reorganize-definitions when transpiling

--- transpile.sh
+++ transpile.sh
@@ -21,7 +21,7 @@ mkdir -p $BUILD_COMMANDS_DIRECTORY
 make clean
 make BUILD_MISSIONPACK=0 BUILD_SERVER=0 BUILD_GAME_SO=1 BUILD_GAME_QVM=0 BUILD_RENDERER_OPENGL2=0 CC=$CC_WRAPPER
 $C2RUST/scripts/convert_build_commands.py $BUILD_COMMANDS_DIRECTORY $CC_DB
-$C2RUST/target/release/c2rust transpile $CC_DB -e -o $OUTPUT_DIR --overwrite-existing
+$C2RUST/target/release/c2rust transpile $CC_DB -e -o $OUTPUT_DIR --overwrite-existing --reorganize-definitions
 
 # Until we implemented custom alignments for globals, we need to fix
 # asm/snapvector.rs manually
