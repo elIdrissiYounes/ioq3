bfbc409ab94fabf5918a80d06e947d4207e6645f
Andrei Homescu <ah@immunant.com>
2019-12-16 13:49:55 -0800

    Modify transpile.sh to accept a second argument with the path to the c2rust binary

--- transpile.sh
+++ transpile.sh
@@ -5,12 +5,13 @@ set -e
 SCRIPT_DIR=$(dirname $(readlink -f $0))
 
 if [[ ! -d "$1" ]]; then
-    echo -e "Usage: $0 <path/to/C2Rust>"
+    echo -e "Usage: $0 <path/to/C2Rust repository> <path/to/c2rust binary>"
     exit 1
 fi
 
-C2RUST=$(readlink -f $1)
-CC_WRAPPER=$C2RUST/scripts/cc-wrappers/cc
+C2RUST_REPO=$(readlink -f $1)
+C2RUST_EXE=$(readlink -f $2)
+CC_WRAPPER=$C2RUST_REPO/scripts/cc-wrappers/cc
 export BUILD_COMMANDS_DIRECTORY=$SCRIPT_DIR/build_commands
 CC_DB=$SCRIPT_DIR/compile_commands.json
 OUTPUT_DIR=$SCRIPT_DIR/quake3-rs
@@ -20,8 +21,8 @@ mkdir -p $BUILD_COMMANDS_DIRECTORY
 
 make clean
 make BUILD_MISSIONPACK=0 BUILD_SERVER=0 BUILD_GAME_SO=1 BUILD_GAME_QVM=0 BUILD_RENDERER_OPENGL2=0 CC=$CC_WRAPPER
-$C2RUST/scripts/convert_build_commands.py $BUILD_COMMANDS_DIRECTORY $CC_DB
-$C2RUST/target/release/c2rust transpile $CC_DB -e -o $OUTPUT_DIR --overwrite-existing --reorganize-definitions
+$C2RUST_REPO/scripts/convert_build_commands.py $BUILD_COMMANDS_DIRECTORY $CC_DB
+$C2RUST_EXE transpile $CC_DB -e -o $OUTPUT_DIR --overwrite-existing --reorganize-definitions
 
 # Until we implemented custom alignments for globals, we need to fix
 # asm/snapvector.rs manually
